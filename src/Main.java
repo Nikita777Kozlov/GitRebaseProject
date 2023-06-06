873c13edc6a806d8e268445934e5e9af58a0b647
ITD\Kazlou_MS <mister.nikita2000@bk.ru>
2023-06-06 12:24:53 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello fixuping and rebasing world!");
+
+        String str = "second commit is going...";
+        System.out.println(str);
     }
 }
